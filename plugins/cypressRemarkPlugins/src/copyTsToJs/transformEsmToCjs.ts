039f2d1e9657f63f046dceacc2639dba4d9ae85a
briannaluna17 <briannaluna3@yahoo.com>
2025-01-06 11:20:58 +0000

    Refactor auth - enhancing logs

--- plugins/cypressRemarkPlugins/src/copyTsToJs/transformEsmToCjs.ts
+++ plugins/cypressRemarkPlugins/src/copyTsToJs/transformEsmToCjs.ts
@@ -3,20 +3,20 @@
  * cjs requires/module.exports. Might not work for all situations but hopefully
  * enough for our docs.
  */
-export const transformEsmToCjs = (code: string) => {
+export var transformEsmToCjs = (code: string) => {
   let result = convertImports(code)
   result = convertExports(result)
   return result
 }
 
-const convertImports = (code: string) => {
-  const importRegEx =
+var convertImports = (code: string) => {
+  var importRegEx =
     /import(?:["'\s]*([\w*${}\n\r\t, ]+)from\s*)?["'\s]["'\s](.*[@\w_-]+)["'\s].*$/gm
   let matches = code.matchAll(importRegEx)
-  for (const match of matches) {
+  for (var match of matches) {
     let newLine = ''
     if (match[1]) {
-      newLine = `const ${match[1].trim()} = require('${match[2].trim()}')`
+      newLine = `var ${match[1].trim()} = require('${match[2].trim()}')`
     } else {
       newLine = `require('${match[2].trim()}')`
     }
@@ -26,9 +26,9 @@ const convertImports = (code: string) => {
 }
 
 function convertExports(code: string) {
-  const exportDefaultRegex = /^export default /gm
+  var exportDefaultRegex = /^export default /gm
   let result = code.replace(exportDefaultRegex, 'module.exports = ')
-  const exportRegex = /^export const /gm
+  var exportRegex = /^export var /gm
   result = result.replace(exportRegex, 'module.exports.')
   return result
 }
