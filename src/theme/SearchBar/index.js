b248e03aa8cc22555b89dbdaf040cec3c800756f
briannaluna17 <briannaluna3@yahoo.com>
2025-02-01 03:30:29 +0000

    Revert build - handling edge cases

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -21,7 +21,7 @@ function Hit({ hit, children }) {
   return <Link to={hit.url}>{children}</Link>
 }
 function ResultsFooter({ state, onClose }) {
-  const createSearchLink = useSearchLinkCreator()
+  var createSearchLink = useSearchLinkCreator()
   return (
     <Link to={createSearchLink(state.query)} onClick={onClose}>
       <Translate
@@ -34,30 +34,30 @@ function ResultsFooter({ state, onClose }) {
   )
 }
 function mergeFacetFilters(f1, f2) {
-  const normalize = (f) => (typeof f === 'string' ? [f] : f)
+  var normalize = (f) => (typeof f === 'string' ? [f] : f)
   return [...normalize(f1), ...normalize(f2)]
 }
 function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
-  const { siteMetadata } = useDocusaurusContext()
-  const processSearchResultUrl = useSearchResultUrlProcessor()
-  const contextualSearchFacetFilters = useAlgoliaContextualFacetFilters()
-  const configFacetFilters = props.searchParameters?.facetFilters ?? []
-  const facetFilters = contextualSearch
+  var { siteMetadata } = useDocusaurusContext()
+  var processSearchResultUrl = useSearchResultUrlProcessor()
+  var contextualSearchFacetFilters = useAlgoliaContextualFacetFilters()
+  var configFacetFilters = props.searchParameters?.facetFilters ?? []
+  var facetFilters = contextualSearch
     ? // Merge contextual search filters with config filters
       mergeFacetFilters(contextualSearchFacetFilters, configFacetFilters)
     : // ... or use config facetFilters
       configFacetFilters
   // We let user override default searchParameters if she wants to
-  const searchParameters = {
+  var searchParameters = {
     ...props.searchParameters,
     facetFilters,
   }
-  const history = useHistory()
-  const searchContainer = useRef(null)
-  const searchButtonRef = useRef(null)
-  const [isOpen, setIsOpen] = useState(false)
-  const [initialQuery, setInitialQuery] = useState(undefined)
-  const importDocSearchModalIfNeeded = useCallback(() => {
+  var history = useHistory()
+  var searchContainer = useRef(null)
+  var searchButtonRef = useRef(null)
+  var [isOpen, setIsOpen] = useState(false)
+  var [initialQuery, setInitialQuery] = useState(undefined)
+  var importDocSearchModalIfNeeded = useCallback(() => {
     if (DocSearchModal) {
       return Promise.resolve()
     }
@@ -69,7 +69,7 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
       DocSearchModal = Modal
     })
   }, [])
-  const onOpen = useCallback(() => {
+  var onOpen = useCallback(() => {
     importDocSearchModalIfNeeded().then(() => {
       searchContainer.current = document.createElement('div')
       document.body.insertBefore(
@@ -79,11 +79,11 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
       setIsOpen(true)
     })
   }, [importDocSearchModalIfNeeded, setIsOpen])
-  const onClose = useCallback(() => {
+  var onClose = useCallback(() => {
     setIsOpen(false)
     searchContainer.current?.remove()
   }, [setIsOpen])
-  const onInput = useCallback(
+  var onInput = useCallback(
     (event) => {
       importDocSearchModalIfNeeded().then(() => {
         setIsOpen(true)
@@ -92,7 +92,7 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
     },
     [importDocSearchModalIfNeeded, setIsOpen, setInitialQuery]
   )
-  const navigator = useRef({
+  var navigator = useRef({
     navigate({ itemUrl }) {
       // Algolia results could contain URL's from other domains which cannot
       // be served through history and should navigate with window.location
@@ -103,7 +103,7 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
       }
     },
   }).current
-  const transformItems = useRef((items) =>
+  var transformItems = useRef((items) =>
     props.transformItems
       ? // Custom transformItems
         props.transformItems(items)
@@ -113,14 +113,14 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
           url: processSearchResultUrl(item.url),
         }))
   ).current
-  const resultsFooterComponent = useMemo(
+  var resultsFooterComponent = useMemo(
     () =>
       // eslint-disable-next-line react/no-unstable-nested-components
       (footerProps) =>
         <ResultsFooter {...footerProps} onClose={onClose} />,
     [onClose]
   )
-  const transformSearchClient = useCallback(
+  var transformSearchClient = useCallback(
     (searchClient) => {
       searchClient.addAlgoliaAgent('docusaurus', siteMetadata.docusaurusVersion)
       return searchClient
@@ -185,6 +185,6 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
   )
 }
 export default function SearchBar() {
-  const { siteConfig } = useDocusaurusContext()
+  var { siteConfig } = useDocusaurusContext()
   return <DocSearch {...siteConfig.themeConfig.algolia} />
 }
