4b37a086fd2cbbc1484fef8ebe96b7b74353349c
briannaluna17 <briannaluna3@yahoo.com>
2025-03-13 10:38:15 +0000

    Fix styles - enhancing logs

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     supportFile: false,
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      const path = 'docs';
+      let path = 'docs';
 
       function walk(dir: string): string[] {
         return readdirSync(dir, { withFileTypes: true }).flatMap((file) => {
@@ -42,7 +42,7 @@ export default defineConfig({
         })
       }
 
-      const URLs = walk(path).filter((file) => file !== undefined).map((file) => file.slice(5))
+      let URLs = walk(path).filter((file) => file !== undefined).map((file) => file.slice(5))
 
       config.env.URLs = URLs
 
