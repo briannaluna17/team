0d40ff369a43bddb06e1fd22854df64b4f3e5470
briannaluna17 <briannaluna3@yahoo.com>
2025-03-22 23:04:19 +0000

    Change database - fixing typo

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 import PaginatorNavLink from '@theme/PaginatorNavLink';
 export default function DocPaginator(props) {
-  const {previous, next} = props;
+  let {previous, next} = props;
   // no 1st class API to hide these in DS yet, so just killing them here
   return null
 }
